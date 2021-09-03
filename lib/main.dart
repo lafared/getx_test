d57fe654278ef4df05bde245a1e077d88c033816
THINKBIG\19002130 <wjdev.iosdev.003@gmail.com>
2021-09-03 12:47:07 +0900

    setState 대신 반응형 상태관리자 Obx 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,13 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  CounterController _counterController = CounterController();
 
   @override
   Widget build(BuildContext context) {
@@ -49,18 +46,22 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Obx(() => Text('${_counterController.count}')),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {_counterController.increment();},
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class CounterController extends GetxController {
+  RxInt count = 0.obs;
+  void increment() {
+    count++;
   }
 }
